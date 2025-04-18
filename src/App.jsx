7045dcb2ebfbf7b65010537309e1705fddaa3c26
xavier <gregorcher34@yahoo.com>
2025-04-18 16:17:57 +0200

    Added JSON to represent data and made components dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Component} from 'react';
+import './App.css';
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import Main from './components/Main.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+        <Footer />
       </div>
-      <h1>Hola Món!</h1>
-      <h2>Some changes applied</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Welcome from './Welcome.jsx';
+import Hotelinfo from './Hotelinfo.jsx';
+
+const Main = () => {
+    return (
+        <main id="wrapper">
+            <Welcome />
+            <Hotelinfo />
+        </main>
+
+    )
+}
+
+export default Main;
